42b0e5b961db8dcb6b68a31c564c11a04cdb63ba
锐 朱 <nicesiri4268@126.com>
2019-08-16 00:28:52 +0800

    upgate

--- test/test/4_1.py
+++ test/test/4_1.py
@@ -2,8 +2,8 @@
 databook = {"siri":'123456',"zhu":'123456',"admin":'123456'}
 databook_2 = databook.copy()
 print(databook_2)
-databook.clear()
 databook_3=dict()
+databook.clear()
 databook_3["name"]=("siri",'siri123456','true','male') #使用元组放置放置信息,键-值 值使用元组表示
 print(databook_3)
 print(databook_3["name"][1].title()) #读取元组内的数据,使用列表的语法读取
