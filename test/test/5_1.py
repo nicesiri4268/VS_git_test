ecd997056785dc3f3ad16df59eb5b11d39882ee0
锐 朱 <nicesiri4268@126.com>
2019-08-17 12:09:22 +0800

    5

---
+++ test/test/5_1.py
@@ -0,0 +1,20 @@
+i=int(input("i = "))
+assert i<=10 ,'i is too bigger ' #测试i是否太大
+for number  in range(0,i):
+    print (number)
+
+strings=["siri","bulexxxfly","redxxxcat","lady","siri","female"]
+for string in strings :
+    if 'siri' in string:
+        index = strings.index(string)
+        strings[index]="zhu"
+print (strings)
+
+strings=["siri","bulexxxfly","redxxxcat","lady","siri","female"]
+
+for index,string in enumerate(strings): 
+    #enumerate() 函数用于将一个可遍历的数据对象(如列表、元组或字符串)组合为一个索引序列
+    #同时列出数据和数据下标，一般用在 for 循环当中。
+    if "siri" in string:
+        strings[index]= "zhu"
+print (strings)
