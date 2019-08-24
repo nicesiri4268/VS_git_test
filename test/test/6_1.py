b685570f0a0faa507a1e7d990b147273e0ed9627
锐 朱 <nicesiri4268@126.com>
2019-08-24 21:09:54 +0800

    7-1

--- test/test/6_1.py
+++ test/test/6_1.py
@@ -1,3 +1,25 @@
 import head_6_1 as h_6
-h_6.test()
+Myname ={}
+h_6.init(Myname)
+h_6.store(Myname,"mange lie hetland")
+print (h_6.lookup (Myname,"middle","lie"))
+h_6.store_1(Myname,"luke skywalker","anakin skywalker")
+#使用 * 来使函数一次输入多个参数
+print (h_6.lookup(Myname, "last","skywalker"))
 
+
+
+#递归实践
+def factorial(n):
+    "递归的基础实现方法"
+    result = n
+    for i in range(1,n):
+        result *=i
+    return result
+def factorial_1(n):
+    if n==1:
+        return 1
+    else :
+        return n*factorial_1(n-1)
+
+print (factorial_1(10),"and",factorial(10))
--- test/test/head_6_1.py
+++ test/test/head_6_1.py
@@ -1,2 +1,38 @@
 def test():
     print ('yes')
+
+def init(data):
+    'data is a dict'
+    data["fist"]={}
+    data["middle"]={}
+    data["last"]={}
+
+def lookup(data,label,name):
+    return data[label].get(name)
+
+def store(data,full_name):
+    names = full_name.split()
+    if len(names)==2:
+        names.insert(1," ")
+    labels = ("fist","middle","last")
+    for label ,name in zip (labels,names):
+        people = lookup(data,label,name)
+        if people:
+            people.append(full_name)
+        else :
+            data[label][name]=[full_name]
+
+def store_1(data,*full_names):
+    "允许多个姓名输入"
+    for full_name in full_names :
+        names = full_name.split()
+        if len(names)==2:
+            names.insert(1," ")
+        labels = ("fist","middle","last")
+        for label ,name in zip (labels,names):
+            people = lookup(data,label,name)
+            if people:
+                people.append(full_name)
+            else :
+                data[label][name]=[full_name]
+    
